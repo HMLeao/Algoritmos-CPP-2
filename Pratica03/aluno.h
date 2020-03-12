4c3fc767b2de2f57f9effb468c47a7fbbc94c041
Henrique Martins <leaomartinshenrique@gmail.com>
2020-03-12 09:24:59 -0300

    Pratica03 finalizada

--- Pratica03/aluno.h
+++ Pratica03/aluno.h
@@ -10,21 +10,34 @@ private:
     string mat;
 public:
     Aluno (){}
-    Aluno (const char * nome, const char * mat) : nome(nome), mat(mat) {}
+    Aluno (string nome, string mat) : nome(nome), mat(mat) {}
     friend class Arranjo<Aluno>;
+
+    // Aluno & operator=(const Aluno & aluno)
+    // {
+    //     return (Aluno &)aluno;
+    // }
+    friend ostream & operator<<(ostream & out, const Aluno & aluno);
 };
 
 template<>
 void Arranjo<Aluno>::set(int idx, const Aluno & aluno) 
 {
+    if(idx > this->tamanho) throw "acesso ilegal: índice maior que o tamanho alocado";
+    if(idx < 0) throw "acesso ilegal: índice menor que zero.";
     this->items[idx] = aluno;
 }
 
-template<>
-void Arranjo<Aluno>::exibir() 
+// template<>
+// void Arranjo<Aluno>::exibir() 
+// {
+//     for(int i=0; i < this->tamanho; i++)
+//     {
+//         cout << i << ": " << this->items[i].mat << " = " << this->items[i].nome << endl;
+//     }
+// }
+
+ostream & operator<<(ostream & out, const Aluno & aluno)
 {
-    for(int i=0; i < this->tamanho; i++)
-    {
-        cout << i << ": " << this->items[i].mat << " = " << this->items[i].nome << endl;
-    }
+     return out << aluno.mat << " = " << aluno.nome;
 }
--- Pratica03/pratica3.cpp
+++ Pratica03/pratica3.cpp
@@ -32,31 +32,50 @@ int main()
     f_arr.set(6,6.8);
     f_arr.exibir();
 
-    // Arranjo<int> ex_tes(10);
-    // try{
-    //     ex_tes.set(-4,5);
-    // }catch(const char * e){
-    //     cout << e << endl;
-    // }
+    Arranjo<int> ex_tes(10);
+    try{
+        ex_tes.set(-4,5);
+    }catch(const char * e){
+        cout << e << endl;
+    }
 
-    // try{
-    //     ex_tes.set(12,8);
-    // }catch(const char * e){
-    //     cout << e << endl;
-    // }
+    try{
+        ex_tes.set(12,8);
+    }catch(const char * e){
+        cout << e << endl;
+    }
 
-    // try{
-    //     ex_tes.set(6,4);
-    // }catch(const char * e){
-    //     cout << e << endl;
-    // }
-    // ex_tes.exibir();
+    try{
+        ex_tes.set(6,4);
+    }catch(const char * e){
+        cout << e << endl;
+    }
+    ex_tes.exibir();
 
     Arranjo<Aluno> turma(3);
-
-    turma.set(0,Aluno("João","1234"));
-    turma.set(1,Aluno("Maria","6734"));
-    turma.set(2,Aluno("José","9368"));
+    try{
+        turma.set(0,Aluno("João","1234"));
+    } catch(const char * e){
+        cout << e << endl;
+    }
+    
+    try{
+        turma.set(1,Aluno("Maria","6734"));
+    } catch(const char * e){
+        cout << e << endl;
+    }
+    
+    try{
+        turma.set(2,Aluno("José","9368"));
+    }catch(const char * e){
+        cout << e << endl;
+    }
+    
+    try{
+        turma.set(12,Aluno("Marcelo","8837"));
+    }catch(const char * e){
+        cout << e << endl;
+    }
 
     turma.exibir(); 
 
